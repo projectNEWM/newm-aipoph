8c514c7286e30214ed4a76d986909a728787d271
logicalmechanism <logical.mechanism@protonmail.com>
2024-02-08 18:13:57 -0800

    cleaning up venv

--- aipoph/scripts/py/data-signing/data-signing/lib/python3.10/site-packages/cose/keys/__init__.py
+++
@@ -1,5 +0,0 @@
-from .ec2 import EC2Key  # noqa: F01
-from .okp import OKPKey  # noqa: F01
-from .rsa import RSAKey  # noqa: F01
-from .symmetric import SymmetricKey  # noqa: F01
-from .cosekey import CoseKey  # noqa: F01
--- aipoph/scripts/py/data-signing/data-signing/lib/python3.10/site-packages/pycardano/crypto/__init__.py
+++
@@ -1,3 +0,0 @@
-# flake8: noqa
-
-from .bip32 import *
